903ae4c4e668d480994bfc7286297f837373ffff
Charles O'Farrell <cofarrell@atlassian.com>
2012-04-12 22:01:11 +1000

    Semi-working, but getting thrashed by ReferenceBot

--- bots/CharlesBot/Bot.scala
+++ bots/CharlesBot/Bot.scala
@@ -13,6 +13,7 @@ class Bot() {
   val rnd = new Random()
   var apocalypse: Int = 0
   var energy: Int = 0
+  var lastMove = 0;
 
   def respond(input: String): String = {
     val (opcode, params) = CommandParser.apply(input)
@@ -20,33 +21,34 @@ class Bot() {
     if (opcode == "React") {
       val oldEnergy = energy
       energy = params("energy").toInt
-      if (energy < oldEnergy) {
-        Cmd("Say", Map("text" -> "Ouch!"))
-      } else {
-        val board = Board(params("view"))
-        var moves = List(Avoid)
-        val heuristics = moves.map(_.move(board))
-        val options = heuristics.foldLeft(IndexedSeq(0, 0, 0, 0, 0, 0, 0, 0, 0)) {
-          case (xs, x) => xs.zip(x).map {
-            case (a, b) => a + b
-          }
+      //if (energy < oldEnergy) Cmd("Say", Map("text" -> "Ouch!"))
+      val board = Board(params("view"))
+      var moves = List(Avoid)
+      val horizon = 15
+      val heuristics = moves.map(m => board.map(horizon, m.weight))
+      val options = heuristics.foldLeft(IndexedSeq(0, 0, 0, 0, 0, 0, 0, 0, 0)) {
+        case (xs, x) => xs.zip(x).map {
+          case (a, b) => a + b
         }
-        val newMove = options.zipWithIndex.sortBy(_._1).map(_._2).last
-        val direction = newMove match {
-          case 0 => XY(-1, -1)
-          case 1 => XY(0, -1)
-          case 2 => XY(1, -1)
-          case 3 => XY(-1, 0)
-          case 4 => XY(0, 0)
-          case 5 => XY(1, 0)
-          case 6 => XY(-1, 1)
-          case 7 => XY(0, 1)
-          case 8 => XY(1, 1)
-        }
-        //println(board.direct + " " + heuristics + " " + direction)
-
-        Cmd("Move", Map("dx" -> direction.x, "dy" -> direction.y))
       }
+      val o2 = options.updated(lastMove, options(lastMove) * 5)
+      val newMove = board.flip(o2, 0.3f).zipWithIndex.sortBy(_._1).map(_._2).last
+      val direction = newMove match {
+        case 0 => XY(-1, -1)
+        case 1 => XY(0, -1)
+        case 2 => XY(1, -1)
+        case 3 => XY(-1, 0)
+        case 4 => XY(0, 0)
+        case 5 => XY(1, 0)
+        case 6 => XY(-1, 1)
+        case 7 => XY(0, 1)
+        case 8 => XY(1, 1)
+      }
+      board.printBoard(horizon)
+      println(heuristics + " " + direction + " " + lastMove)
+      lastMove = newMove
+
+      Cmd("Move", Map("dx" -> direction.x, "dy" -> direction.y))
     }
 
     else if (opcode == "Welcome") {
@@ -83,130 +85,136 @@ object CommandParser {
 
 object Cmd {
 
-  def main(args: Array[String]) {
-    val me = Pos(31 * 31 / 2)
-    for (y <- 0 until 31) {
-      for (x <- 0 until 31) {
-        val p = Pos((y * 31) + x)
-        //print(p.toXY)
-        print(me.distance(p).toString.substring(0, 1))
-        print(" ")
-      }
-      println("")
-    }
-    println()
-  }
-
   def apply(cmd: String, map: Map[String, Any]) = cmd + "(" + map.toList.map(a => a._1 + "=" + a._2).mkString(",") + ")"
 }
 
 object Board {
   val width = 31
+  val types = List(Empty, Master, MiniMe, Slave, Enemy, Wall, Fluppet, Snorg, Zugar, Toxifera, Hidden).map(s => (s.c, s)).toMap
 
-  def apply(view: String) = {
-    val b = view.map {
-      case '_' => Empty
-      case 'M' => Master
-      case 'm' => Enemy
-      case 'S' => Slave
-      case 's' => Enemy
-      case 'P' => Zugar
-      case 'p' => Toxifera
-      case 'B' => Fluppet
-      case 'b' => Snorg
-      case 'W' => Wall
-      case '?' => Hidden
-    }
-    new Board(b)
-  }
+  def apply(view: String) = new Board(view.map(types(_)))
 }
 
 
 trait Move {
-
-  def move(b: Board): IndexedSeq[Int] = {
-    // TODO Use direct2 to look further ahead
-    // TODO Only return 9 values instead of 18 etc
-    flip(b.direct.map(weight))
-  }
-
-  /**
-   * Adds positive weights to the opposite of negative squares - so we RUN AWAY!!!
-   */
-  def flip(i: IndexedSeq[Int]) = i.map(math.min(_, 0) * -1).reverse.zip(i).map {
-    case (a, b) => a + b
-  }
 
   def weight(s: S): Int = 0
 }
 
 object Avoid extends Move {
   override def weight(s: S) = s match {
-    case Fluppet => 1
-    case Zugar => 1
-    case Wall => -1
-    case Master => -1 // Don't stand still
+    case Fluppet => 13
+    case Zugar => 12
+    case Wall => -5
+    case Slave => -11
+    case Enemy => -10
     case Toxifera => -2
-    case Snorg => -3
+    case Snorg => -10
     case _ => 0
   }
 }
+
+abstract class S(val c: Char)
+
+case object Empty extends S('_')
+
+case object Master extends S('M')
+
+case object Slave extends S('m')
+
+case object MiniMe extends S('S')
+
+case object Enemy extends S('s')
+
+case object Wall extends S('W')
+
+case object Fluppet extends S('B')
+
+case object Snorg extends S('b')
+
+case object Zugar extends S('P')
+
+case object Toxifera extends S('p')
+
+case object Hidden extends S('?')
 
 case class Pos(i: Int) {
 
   import Board.width
 
-  def toXY = XY(i % width, i / width)
+  val toXY = XY(i % width, i / width)
 
-  def distance(p: Pos) = {
+  def vector(p: Pos) = {
     val us = toXY
     val them = p.toXY
     val x = them.x - us.x
     val y = them.y - us.y
-    math.sqrt(x * x + y * y)
+    def round(i: Int): Int = if (i == 0) 0 else math.round(i / math.abs(i))
+    val dir = toDirection(round(x), round(y))
+    val dist = math.round(math.sqrt(x * x + y * y))
+    (dir, dist)
   }
+
+  def toDirection(x: Int, y: Int) = (x, y) match {
+    case (-1, -1) => 0
+    case (0, -1) => 1
+    case (1, -1) => 2
+    case (-1, 0) => 3
+    case (0, 0) => 4
+    case (1, 0) => 5
+    case (-1, 1) => 6
+    case (0, 1) => 7
+    case (1, 1) => 8
+  }
+
 }
 
 class Board(s: IndexedSeq[S]) {
 
   import Board.width
 
-  def get(i: Int) = s((width * width / 2) + i)
-
   def direct_(m: Int) = {
     val r = (i: Int) => (-m to m).map(_ + i)
-    (-m to m).flatMap(x => r(x * width))
+    (-m to m).flatMap(x => r(x * width)) map (_ + (width * width / 2))
   }
 
-  def directGet(m: Int) = direct_(m) map (get(_))
+  def directGet(m: Int) = direct_(m) map (s(_))
+
+
+  def printBoard(m: Int) {
+    val b = directGet(m)
+    val n = (m * 2) + 1
+
+    for (y <- 0 until n) {
+      for (x <- 0 until n) {
+        print(b(x + (n * y)).c)
+      }
+      println("")
+    }
+  }
+
 
   def direct = directGet(1)
 
-  def direct2 = directGet(2)
+  def map(i: Int, f: S => Int) = {
+    val me = Pos(width * width / 2)
+    val a = direct_(i) map {
+      p => {
+        val v = me.vector(Pos(p))
+        (v._1, f(s(p)) * math.pow(i - v._2 + 1, 5))
+      }
+    } groupBy (_._1) mapValues (_.map(_._2).sum.toInt)
+    (0 to 8).map(i => a.getOrElse(i, 0))
+  }
+
+  /**
+   * Adds positive weights to the opposite of negative squares - so we RUN AWAY!!!
+   */
+  def flip(i: IndexedSeq[Int], f: Float) = i.map(m => math.round(m * -f)).reverse.zip(i).map {
+    case (a, b) => a + b
+  }
 
 }
-
-sealed trait S
-
-case object Empty extends S
-
-case object Master extends S
-
-case object Slave extends S
-
-case object Enemy extends S
-
-case object Wall extends S
-
-case object Fluppet extends S
-
-case object Snorg extends S
-
-case object Zugar extends S
-
-case object Toxifera extends S
-
-case object Hidden extends S
 
 case class XY(x: Int, y: Int)
 
